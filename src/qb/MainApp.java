43188c52fc4d223d57cc9ef55d256392ce51fb3c
qelphybox <qelphybox@gmail.com>
2014-12-18 01:05:07 +0400

    Added Close event //TODO EnterButton OnAction

--- src/qb/MainApp.java
+++ src/qb/MainApp.java
@@ -4,17 +4,20 @@ import java.io.IOException;
 
 import qb.login.view.LogInWinController;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class MainApp extends Application {
 
 	private Stage primaryStage;
 	private BorderPane rootLayout;
+	private boolean isDialogClosed = false;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -24,12 +27,14 @@ public class MainApp extends Application {
 		initRootLayout();
 		showTestWindow();
 		showLoginDialog();
+
 	}
-	
+
 	public void initRootLayout() {
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("testwindow/view/RootLayout.fxml"));
+			loader.setLocation(MainApp.class
+					.getResource("testwindow/view/RootLayout.fxml"));
 			rootLayout = loader.load();
 
 			Scene scene = new Scene(rootLayout);
@@ -39,45 +44,58 @@ public class MainApp extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void showTestWindow() {
-		
+
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("testwindow/view/TestWindow.fxml"));
+			loader.setLocation(MainApp.class
+					.getResource("testwindow/view/TestWindow.fxml"));
 			AnchorPane testWindow = loader.load();
-			
+
 			rootLayout.setCenter(testWindow);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public boolean showLoginDialog(){
-		
+
+	public boolean showLoginDialog() {
+
 		try {
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(MainApp.class.getResource("login/view/LogInWin.fxml"));
+			loader.setLocation(MainApp.class
+					.getResource("login/view/LogInWin.fxml"));
 			AnchorPane page = loader.load();
-			
+
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Login");
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.initOwner(primaryStage);
 			Scene scene = new Scene(page);
 			dialogStage.setScene(scene);
-			
+
 			LogInWinController controller = loader.getController();
 			controller.setDialogStage(dialogStage);
+
+			EventHandler<WindowEvent> closingEvent = new EventHandler<WindowEvent>() {
+
+				public void handle(WindowEvent event) {
+					isDialogClosed = true;
+				}
+			};
+
+			dialogStage.setOnCloseRequest(closingEvent);
 			dialogStage.showAndWait();
-			
+
+			if (isDialogClosed)
+				primaryStage.close();
+
 			return controller.isOkClicked();
-		
+
 		} catch (IOException e) {
 			e.printStackTrace();
 			return false;
 		}
-	
 	}
 
 	public static void main(String[] args) {
--- src/qb/login/view/LogInWinController.java
+++ src/qb/login/view/LogInWinController.java
@@ -2,8 +2,6 @@ package qb.login.view;
 
 import org.controlsfx.dialog.Dialogs;
 
-import com.sun.webkit.ContextMenu.ShowContext;
-
 import qb.login.model.LogPass;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
@@ -11,58 +9,60 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LogInWinController {
-	
+
+	private boolean isClosed = false;
 	@FXML
 	private TextField loginField;
 	@FXML
 	private PasswordField passwordField;
-	
+
 	private Stage dialogStage;
 	private LogPass logpass;
 	private boolean okClicked = false;
-	
+
 	@FXML
-	private void initialize(){
+	private void initialize() {
 	}
-	
-	public void setDialogStage(Stage dialogStage){
+
+	public void setDialogStage(Stage dialogStage) {
 		this.dialogStage = dialogStage;
 	}
-	
-	 public boolean isOkClicked() {
-	        return okClicked;
-	 }
-	 
+
+	public boolean isOkClicked() {
+		return okClicked;
+	}
+
 	@FXML
-	private void handleOk(){
-		
-		logpass.setLogin(loginField.getText());
-		logpass.setPassword(passwordField.getText());
-		
+	private void handleOk() {
+
+		if (isInputValid()) {
+			logpass.setLogin(loginField.getText());
+			logpass.setPassword(passwordField.getText());
+		}
+
 		okClicked = true;
 		dialogStage.close();
 	}
-	
+
 	private boolean isInputValid() {
-		
+
 		String errorMessage = "";
 		if (loginField.getText() == null || loginField.getText().length() == 0)
-			errorMessage += "Введите логин";
-		
-		if (passwordField.getText() == null || passwordField.getText().length() == 0 )
+			errorMessage += "Введите логин \n";
+
+		if (passwordField.getText() == null
+				|| passwordField.getText().length() == 0)
 			errorMessage += "Введите пароль";
-		
+
 		if (errorMessage.length() == 0) {
 			return true;
 		} else {
-			
-			Dialogs.create()
-				.title("Ошибка")
-				.masthead("Ошибка ввода")
-				.message(errorMessage)
-				.showError();
+
+			Dialogs.create().title("Ошибка").masthead("Ошибка ввода")
+					.message(errorMessage).showError();
 			return false;
-		}
 			
+		}
+
 	}
 }
